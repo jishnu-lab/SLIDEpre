1835542d0a6ae36eafa70ba75d5d215eb4bb5c12
aerosengart <aelr@umich.edu>
2022-05-26 09:31:10 -0400

    betabma casting

--- R/betaBMA.R
+++ R/betaBMA.R
@@ -27,9 +27,9 @@ betaBMA <- function(x, y, er_res, imps, imps_z, estim = "HPM") {
   ## Step 1: Important Clusters
   ## create initial probabilities vector
   z_imp <- prior_z[, imps_z] %>%
-    as.data.frame()
+    as.matrix()
   loadings <- er_res$A[, imps_z] %>%
-    as.data.frame()
+    as.matrix()
   z_imp_probs <- rep(0, ncol(z_imp))
 
   ## make initial inclusion probabilities
