e68c99c749c19a42cb9410eda18a82e8594ce64d
aerosengart <aelr@umich.edu>
2022-06-21 15:08:11 -0400

    data

---
+++ R/x_data.R
@@ -0,0 +1,7 @@
+##' Gene expression data from Var50.
+##'
+##' Example data for the Essential Regression vignette.
+##'
+##' @format A data frame with 24 rows and 800 columns.
+##'
+"x_data"
---
+++ R/y_data.R
@@ -0,0 +1,7 @@
+##' Response data from Var50.
+##'
+##' Example data for the Essential Regression vignette.
+##'
+##' @format A data frame with 24 rows and 1 column.
+##'
+"y_data"
